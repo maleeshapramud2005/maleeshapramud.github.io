754298b09651aa14c3749565f522dafd1104102b
maleeshapramud2005 <75854465+maleeshapramud2005@users.noreply.github.com>
2022-08-26 17:39:05 +0530

    Update 2.0 fiverr

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ document.querySelector(".light").addEventListener("click", function() {
   document.querySelector(".down").style.borderColor = "#01FEFF"
   document.querySelector(".down").style.backgroundColor = "#01FEFF"
   document.body.style.backgroundImage = "url('bglight.jpg')"
+  
 
   var x = document.querySelectorAll(".black");
   var i;
